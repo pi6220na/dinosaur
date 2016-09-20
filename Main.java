5e773e8dea10a808dd50135b58f167657b790916
pi6220na <myrlin2002@hotmail.com>
2016-09-20 11:35:45 -0500

    first version of dinosaur heh

---
+++ Main.java
@@ -0,0 +1,21 @@
+package com.wolfe;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+        dinosaur t_rex = new dinosaur();
+        dinosaur velociraptor = new dinosaur();
+        dinosaur scary = new dinosaur();
+
+        t_rex.roar();
+        velociraptor.roar();
+        scary.roar();
+
+        // comment
+
+        // comment two
+
+    }
+}
